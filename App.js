decd43f5812c49936ad6790676afb3fc9b1d08a0
kuvii <kuvilet@gmail.com>
2022-09-29 10:36:14 +0100

    Feat: Presentacion de los elementos

--- App.js
+++ App.js
@@ -3,7 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <View style={styles.topContainer}>
+        <Text>imagen</Text>
+        <Text>Text</Text>
+        </View>
+      <View style={styles.centerContainer}>
+        <Text>Presentation</Text>
+        </View>
+      <View style={styles.bottomContainer}>
+        <Text>Qr</Text>
+        </View>
+
     </View>
   );
 }
@@ -14,5 +24,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  topContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    borderWidth: 4,
+    borderColor: 'yellow',
+    width:'100%'
+  },
+  centerContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 4,
+    borderColor: 'green',
+    width: '100%'
+  },
+  bottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 4,
+    borderColor: 'blue',
+    width: '100%'
   }
 });
